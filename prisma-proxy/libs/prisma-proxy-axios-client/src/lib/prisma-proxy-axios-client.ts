9822095e3ac7da14d34327378325380c213fc040
jacksloan <jbsloan10@gmail.com>
2021-12-17 17:43:07 -0600

    fix: `Buildable libraries cannot import or export from non-buildable libraries`

--- prisma-proxy/libs/prisma-proxy-axios-client/src/lib/prisma-proxy-axios-client.ts
+++ prisma-proxy/libs/prisma-proxy-axios-client/src/lib/prisma-proxy-axios-client.ts
@@ -1,5 +1,12 @@
 import axios, { Axios } from 'axios';
-import { PrismaProxy } from '@prisma-proxy/shared';
+
+export type FilterNotStartingWith<
+  T,
+  Prefix extends string
+> = T extends `${Prefix}${infer _Rest}` ? never : T;
+
+// remove all keys starting with '$' prefix leaving only entity names
+export type PrismaProxy<T> = Pick<T, FilterNotStartingWith<keyof T, '$'>>;
 
 export function createAxiosClient<PrismaClient>(options: {
   baseURL: string;
--- prisma-proxy/libs/shared/src/index.ts
+++ prisma-proxy/libs/shared/src/index.ts
@@ -1,7 +1,1 @@
-export type FilterNotStartingWith<
-  T,
-  Prefix extends string
-> = T extends `${Prefix}${infer _Rest}` ? never : T;
 
-// remove all keys starting with '$' prefix leaving only entity names
-export type PrismaProxy<T> = Pick<T, FilterNotStartingWith<keyof T, '$'>>;
