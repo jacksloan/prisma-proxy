5e2c64231bb81abe5ff8ba9799adb12a8991fcc5
jacksloan <jbsloan10@gmail.com>
2021-12-17 17:29:41 -0600

    move prisma proxy axios src to nrwl workspace

--- prisma-proxy/libs/prisma-proxy-axios-client/src/lib/prisma-proxy-axios-client.spec.ts
+++ prisma-proxy/libs/prisma-proxy-axios-client/src/lib/prisma-proxy-axios-client.spec.ts
@@ -1,7 +1,5 @@
-import { prismaProxyAxiosClient } from './prisma-proxy-axios-client';
-
 describe('prismaProxyAxiosClient', () => {
   it('should work', () => {
-    expect(prismaProxyAxiosClient()).toEqual('prisma-proxy-axios-client');
+    // TODO
   });
 });
--- prisma-proxy/libs/prisma-proxy-axios-client/src/lib/prisma-proxy-axios-client.ts
+++ prisma-proxy/libs/prisma-proxy-axios-client/src/lib/prisma-proxy-axios-client.ts
@@ -1,3 +1,31 @@
-export function prismaProxyAxiosClient(): string {
-  return 'prisma-proxy-axios-client';
+import axios, { Axios } from 'axios';
+import { PrismaProxy } from '@prisma-proxy/shared';
+
+export function createAxiosClient<PrismaClient>(options: {
+  baseURL: string;
+  axios?: Axios;
+}): PrismaProxy<PrismaClient> {
+  return new Proxy(
+    {},
+    {
+      get(_target: any, prismaEntity: string, _receiver) {
+        return new Proxy(
+          {},
+          {
+            get(_target: any, prismaAction: string, _receiver) {
+              return async (prismaArgs: any) => {
+                const response = await (options?.axios || axios).request({
+                  method: 'POST',
+                  url: `/${prismaEntity}/${prismaAction}`,
+                  baseURL: options.baseURL,
+                  data: prismaArgs,
+                });
+                return response.data;
+              };
+            },
+          }
+        );
+      },
+    }
+  );
 }
