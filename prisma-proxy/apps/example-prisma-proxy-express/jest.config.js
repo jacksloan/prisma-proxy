9c636d715e975b05c284f05cdbfbbaa35a2d973f
jacksloan <jbsloan10@gmail.com>
2021-12-17 17:11:23 -0600

    conver to nx workspace: initial commit

---
+++ prisma-proxy/apps/example-prisma-proxy-express/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'example-prisma-proxy-express',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/example-prisma-proxy-express',
+};
