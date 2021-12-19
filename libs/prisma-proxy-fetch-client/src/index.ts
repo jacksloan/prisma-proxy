f65c4a94e4b738e30687d87cf6f279d4f8622464
jacksloan <jbsloan10@gmail.com>
2021-12-19 04:11:55 -0600

    allow user to provide their own fetch function

--- libs/prisma-proxy-fetch-client/src/index.ts
+++ libs/prisma-proxy-fetch-client/src/index.ts
@@ -6,20 +6,24 @@ export type FilterNotStartingWith<
 // remove all keys starting with '$' prefix leaving only entity names
 export type PrismaProxy<T> = Pick<T, FilterNotStartingWith<keyof T, '$'>>;
 
-export function createFetchClient<PrismaClient>(
-  baseUrl: string,
-  requestOptions?: () => RequestInit | Promise<RequestInit>
-): PrismaProxy<PrismaClient> {
+export function createFetchClient<PrismaClient>(opt: {
+  baseUrl: string;
+  requestOptions?: () => RequestInit | Promise<RequestInit>;
+  fetch?: (input: RequestInfo, init?: RequestInit) => Promise<Response>;
+}): PrismaProxy<PrismaClient> {
   const newProxy = (
     handler: (_target: any, prop: string, _receiver: any) => any
   ) => new Proxy({}, { get: handler });
   return newProxy((_, prismaEntity) =>
     newProxy((_, prismaAction) => async (body: any) => {
-      const res = await fetch(`${baseUrl}/${prismaEntity}/${prismaAction}`, {
-        method: 'POST',
-        body,
-        ...(requestOptions ? await requestOptions() : {}),
-      });
+      const res = await (opt.fetch || fetch)(
+        `${opt.baseUrl}/${prismaEntity}/${prismaAction}`,
+        {
+          method: 'POST',
+          body,
+          ...(opt.requestOptions ? await opt.requestOptions() : {}),
+        }
+      );
       return res.json();
     })
   );
